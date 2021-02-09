59d292d4051eecfcf3c7612441203e2c0e6a4a87
esswambui <estherwambuik3@gmail.com>
2021-02-09 13:53:11 +0000

    addes holberton.h

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,11 +1,4 @@
-/**
- * _putchar - writes to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and error is set appropriately.
- */
-int _putchar(char c);
+#include "holberton.h"
 
 /**
  * main - print Holberton from function _putchar
