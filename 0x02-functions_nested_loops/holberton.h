fa4ab0a566c364333b0bfb06b29601313a87b073
esswambui <estherwambuik3@gmail.com>
2021-02-10 11:58:07 +0000

    updatef

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -58,4 +58,14 @@ int print_sign(int n);
 int _abs(int j);
 
 
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: integer argument
+ * Return: last digit of number
+ */
+
+int print_last_digit(int n);
+
+
+
 #endif
